8206e69c899cc71d12379be429742f1899fed7a3
Abdellah Bounab <abounab@c1r1p1.1337.ma>
2024-04-28 19:55:36 +0100

    adding the  philo mandatory part

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abounab <abounab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 19:50:54 by abounab           #+#    #+#             */
+/*   Updated: 2024/04/28 19:51:26 by abounab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+#endif
